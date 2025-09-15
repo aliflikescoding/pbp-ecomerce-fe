233f998ef931a1743410df9a3cd48ed6bad8da39
Alif Wibowo <alif.likes.coding@gmail.com>
2025-09-15 08:33:18 +0700

    server init 2

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || "5000";
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  })
+);
+
+app.use("/uploads", express.static("public/uploads"));
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(cookieParser());
+
+const apiRouter = express.Router();
+
+app.use("/api", apiRouter);
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
